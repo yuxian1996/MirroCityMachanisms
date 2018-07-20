f25e944fcabcc07f1b27ce5d913f1ce10fa7c9c5
Yuxian Deng <u1166596@utah.edu>
2018-07-19 19:47:31 -0600

    Update

--- Source/Machanisms/MyCharacter.cpp
+++ Source/Machanisms/MyCharacter.cpp
@@ -9,6 +9,10 @@ AMyCharacter::AMyCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	mMaxWalkSpeed = 600.0f;
+	mMaxSlope = 45.0f;
+	mMaxStepHeight = 50.0f;
+	mGravity = FVector(0, 0, -980.0f);
 }
 
 // Called when the game starts or when spawned
@@ -18,7 +22,7 @@ void AMyCharacter::BeginPlay()
 
 	mpMovement = GetCharacterMovement();
 	mpCapsule = GetCapsuleComponent();
-	
+	mGravityNormal = mGravity.GetSafeNormal();
 }
 
 // Called every frame
@@ -39,45 +43,62 @@ void AMyCharacter::UpdateWalk()
 {
 	FVector inputVector = mpMovement->GetLastInputVector();
 	FVector originalVelocity = inputVector * mMaxWalkSpeed;
+
 	if (UWorld* world = GetWorld())
 	{
-		FVector deltaMovement = originalVelocity * world->TimeSeconds;
+		FVector deltaMovement = originalVelocity * world->GetDeltaSeconds();
 		FVector location = GetActorLocation();
-		FVector newLocation = location + deltaMovement;
-
+		FVector newLocation = location + deltaMovement;		
 		// check if next location is blocked
 		FCollisionQueryParams queryParams;
 		// ignore self
 		queryParams.AddIgnoredActor(this);
 		FCollisionResponseParams responseParams;
+	
 		// a little bit up
-		if (!world->OverlapAnyTestByChannel(newLocation - mGravity.Normalize() * 5, GetActorRotation().Quaternion(), ECollisionChannel::ECC_Visibility,
+		if (!world->OverlapAnyTestByChannel(newLocation - mGravityNormal * 1, GetActorRotation().Quaternion(), ECollisionChannel::ECC_Visibility,
 			FCollisionShape::MakeCapsule(mpCapsule->GetScaledCapsuleRadius(), mpCapsule->GetScaledCapsuleHalfHeight()),
 			queryParams, responseParams))
 		{
+			FVector end = newLocation + mGravityNormal * (mMaxStepHeight + mpCapsule->GetScaledCapsuleHalfHeight());
+			FHitResult hitResult;
+			if (world->LineTraceSingleByChannel(hitResult, newLocation, end, ECC_Visibility, queryParams, responseParams))
+			{
+				// go down
+				SetActorLocation(hitResult.Location - mGravityNormal * mpCapsule->GetScaledCapsuleHalfHeight());
+			}
+			else
+			{
+				// move on plane
+				SetActorLocation(newLocation);
+			}
 			mpMovement->Velocity = originalVelocity;
-			mpMovement->UpdateBasedMovement(world->TimeSeconds);
-			//SetActorLocation(newLocation);
+			//mpMovement->UpdateBasedMovement(world->TimeSeconds);
+			UE_LOG(LogTemp, Log, TEXT("Walk to %f, %f, %f"), newLocation.X, newLocation.Y, newLocation.Z);
 		}
 		else
 		{
 			FVector originalDirection = originalVelocity.GetSafeNormal();
-			FVector bottom = location + mGravity.GetSafeNormal() * (mpCapsule->GetScaledCapsuleHalfHeight() - 1.0f);
+			FVector bottom = location + mGravityNormal * (mpCapsule->GetScaledCapsuleHalfHeight() - 1.0f);
 			FVector end = bottom + originalDirection * mpCapsule->GetScaledCapsuleRadius() + deltaMovement;
 			// try to change direction or stop
 			FHitResult hitResult;
 			if (world->LineTraceSingleByChannel(hitResult, bottom, end, ECollisionChannel::ECC_Visibility, queryParams, responseParams))
 			{
 				FVector planeVector = FVector::VectorPlaneProject(hitResult.Normal, -mGravityNormal);
-				float angle = FMath::Acos(planeVector.Size());
+				float angle = FMath::Abs(FMath::Acos(planeVector.Size()));
 
-				if (angle < mMaxSlope)
+				UE_LOG(LogTemp, Log, TEXT("Angle = %f"), angle);
+				if (angle < mMaxSlope && angle > 0)
 				{
 					// walk on slope 
 					FVector axis = FVector::CrossProduct(originalDirection, -mGravityNormal);
 					FVector newVelocity = originalVelocity.RotateAngleAxis(angle, axis);
 					mpMovement->Velocity = newVelocity;
-					mpMovement->UpdateBasedMovement(world->TimeSeconds);
+					//mpMovement->UpdateBasedMovement(world->TimeSeconds);
+					SetActorLocation(location + newVelocity *  world->GetDeltaSeconds());
+					UE_LOG(LogTemp, Log, TEXT("Walk on slope"));
+
 				}
 				else
 				{
@@ -90,7 +111,9 @@ void AMyCharacter::UpdateWalk()
 						world->LineTraceSingleByChannel(hitResult, end, end + mGravityNormal * mMaxStepHeight,
 							ECollisionChannel::ECC_Visibility, queryParams, responseParams);
 						mpMovement->Velocity = originalVelocity;
-						SetActorLocation(location - mGravityNormal * (mMaxStepHeight - hitResult.Distance));
+						SetActorLocation(newLocation - mGravityNormal * (mMaxStepHeight - hitResult.Distance));
+						UE_LOG(LogTemp, Log, TEXT("Walk on stairs"));
+
 					}
 
 				}
--- Source/Machanisms/MyCharacter.h
+++ Source/Machanisms/MyCharacter.h
@@ -34,38 +34,46 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UFUNCTION(BlueprintPure)
+	UFUNCTION(BlueprintPure, Category ="Walk" )
 	float GetMaxWalkSpeed() { return mMaxWalkSpeed; };
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, Category = "Walk")
 	void SetMaxWalkSpeed(float iMaxWalkSpeed) { mMaxWalkSpeed = iMaxWalkSpeed; };
 
-	UFUNCTION(BlueprintGetter, Category = "Walk")
+	UFUNCTION(BlueprintPure, Category = "Walk")
 		float GetMaxStepHeight() { return mMaxStepHeight; };
-	UFUNCTION(BlueprintSetter, Category = "Walk")
-		void SetMaxStepHeight(float iMaxStepHeight) { mMaxStepHeight = iMaxStepHeight; };
+	//UFUNCTION(BlueprintCallable, Category = "Walk")
+		//void SetMaxStepHeight(float iMaxStepHeight) { mMaxStepHeight = iMaxStepHeight; };
 
-	UFUNCTION(BlueprintGetter, Category = "Movement")
+	UFUNCTION(BlueprintPure, Category = "Movement")
 		FVector GetGravity() { return mGravity; };
-	UFUNCTION(BlueprintSetter, Category = "Movement")
-		void SetGravity(FVector iGravity) { mGravity = iGravity; };
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+		void SetGravity(FVector iGravity) { mGravity = iGravity; mGravityNormal = mGravity.GetSafeNormal(); };
+	UFUNCTION(BlueprintPure, Category = "Movement")
+		FVector GetGravityNormal() { return mGravityNormal; };
 
-	UFUNCTION(BlueprintGetter, Category = "Walk")
+	UFUNCTION(BlueprintPure, Category = "Walk")
 		float GetMaxSlope() { return mMaxSlope; };
-	UFUNCTION(BlueprintSetter, Category = "Walk")
-		void SetMaxSlope(float iMaxSlope) { mMaxSlope = iMaxSlope; };
+	//UFUNCTION(BlueprintCallable, Category = "Walk")
+		//void SetMaxSlope(float iMaxSlope) { mMaxSlope = iMaxSlope; };
 
 private:
 	UCharacterMovementComponent* mpMovement;
 	UCapsuleComponent* mpCapsule;
 
 	// Max speed when walking
-	UPROPERTY(BlueprintGetter = GetMaxWalkSpeed, BlueprintSetter = SetMaxWalkSpeed, Category = "Walk")
+	UPROPERTY(EditDefaultsOnly,  BlueprintGetter = GetMaxWalkSpeed, BlueprintSetter = SetMaxWalkSpeed, Category = "Walk")
 	float mMaxWalkSpeed;
+
 	// Max step height
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter = GetMaxStepHeight, Category = "Walk" )
 	float mMaxStepHeight;
+
 	// Max slope angle
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter = GetMaxSlope, Category = "Walk")
 	float mMaxSlope;
+
 	// Current Gravity
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter = GetGravity, BlueprintSetter = SetGravity, Category = "Movement", meta=(DisplayName = "Gravity"))
 	FVector mGravity;
 	// Normalized Gravity
 	FVector mGravityNormal;
